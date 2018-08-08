9e3e5f90564692a9f96ec83c20e03515b6322ab3
chenqiu1024 <godspeed1024@gmail.com>
2018-08-08 17:37:17 +0800

    enumerateInBoxes;

--- ISOBMFF-Dump/main.cpp
+++ ISOBMFF-Dump/main.cpp
@@ -34,8 +34,6 @@
 #include <fstream>
 #include <cstring>
 
-using namespace ISOBMFF;
-
 inline int32_t readLittleEndianInt32(const void* ptr) {
     const uint8_t* bytes = (const uint8_t*)ptr;
     int32_t ret = bytes[0] | (bytes[1] << 8) | (bytes[2] << 16) | (bytes[3] << 24);
@@ -59,6 +57,58 @@ inline int16_t readBigEndianInt16(const void* ptr) {
     int32_t ret = bytes[1] | (bytes[0] << 8);
     return (int16_t)ret;
 }
+
+void enumerateInBoxes(const void* data, int size, void(*callback)(void* context, uint32_t boxType, uint8_t* boxData, int boxSize, bool* stop), void* context);
+
+void handleBox(void* context, uint32_t boxType, uint8_t* boxData, int boxSize, bool* stop);
+
+void enumerateInBoxes(const void* data, int size, void(*callback)(void* context, uint32_t boxType, uint8_t* boxData, int boxSize, bool* stop), void* context) {
+    bool stop = false;
+    uint8_t* pRead = (uint8_t*)data;
+    for (int iRead = 0; iRead < size;)
+    {
+        uint32_t* pReadInt = (uint32_t*)pRead;
+        int boxSize = readBigEndianInt32(pReadInt++);
+        uint32_t boxType = (uint32_t)readLittleEndianInt32(pReadInt++);
+        
+        if (NULL != callback)
+        {
+            callback(context, boxType, (uint8_t*)pReadInt, boxSize - 2 * (int)sizeof(uint32_t), &stop);
+            if (stop)
+                break;
+        }
+        
+        pRead += boxSize;
+        iRead += (2 * (int)sizeof(uint32_t) + boxSize);
+    }
+}
+
+void handleBox(void* context, uint32_t boxType, uint8_t* boxData, int boxSize, bool* stop) {
+    printf("boxType=0x%x, boxSize=%d, boxData=0x%lx, context=0x%lx\n", boxType, boxSize, (long)boxData, (long)context);
+    *stop = false;
+    switch (boxType)
+    {
+        case MADV_MP4_USERDATA_MADV:
+            enumerateInBoxes(boxData, boxSize, handleBox, context);
+            break;
+        case MADV_MP4_USERDATA_TAG_TYPE:
+            break;
+        case MADV_MP4_USERDATA_BEAUTY_TYPE:
+            break;
+        case MADV_MP4_USERDATA_CAMERA_INFO_TYPE:
+            break;
+        case MADV_MP4_USERDATA_GPS_TYPE:
+            break;
+        case MADV_MP4_USERDATA_LUT_TYPE:
+            break;
+        case MADV_MP4_USERDATA_GYRO_TYPE:
+            break;
+        default:
+            break;
+    }
+}
+
+using namespace ISOBMFF;
 
 class CustomBox: public ISOBMFF::Box
 {
@@ -149,35 +199,8 @@ int main( int argc, const char * argv[] )
                         std::vector<uint8_t> userData = subBox->GetData();
                         int boxBytesSize = (int)userData.size();
                         std::cout << "udta length = " << boxBytesSize << std::endl;
-                        uint8_t* pRead = userData.data();
-                        for (int iRead = 0; iRead < boxBytesSize;)
-                        {
-                            uint32_t* pReadInt = (uint32_t*)pRead;
-                            uint32_t size = (uint32_t)readBigEndianInt32(pReadInt++);
-                            uint32_t type = (uint32_t)readLittleEndianInt32(pReadInt++);
-                            pRead = (uint8_t*)pReadInt;
-                            switch (type)
-                            {
-                                case MADV_MP4_USERDATA_MADV:
-                                    break;
-                                case MADV_MP4_USERDATA_TAG_TYPE:
-                                    break;
-                                case MADV_MP4_USERDATA_BEAUTY_TYPE:
-                                    break;
-                                case MADV_MP4_USERDATA_CAMERA_INFO_TYPE:
-                                    break;
-                                case MADV_MP4_USERDATA_GPS_TYPE:
-                                    break;
-                                case MADV_MP4_USERDATA_LUT_TYPE:
-                                    break;
-                                case MADV_MP4_USERDATA_GYRO_TYPE:
-                                    break;
-                                default:
-                                    break;
-                            }
-                            pRead += (size - 2 * sizeof(uint32_t));
-                            iRead += (2 * sizeof(uint32_t) + size);
-                        }
+                        enumerateInBoxes(userData.data(), boxBytesSize, handleBox, NULL);
+                        return EXIT_SUCCESS;
                     }
                 }
             }
@@ -185,9 +208,9 @@ int main( int argc, const char * argv[] )
 //        std::cout << *file << std::endl << std::endl;
     }
 
-    #if defined( _WIN32 ) && defined( _DEBUG )
-    getchar();
-    #endif
+//    #if defined( _WIN32 ) && defined( _DEBUG )
+//    getchar();
+//    #endif
     
     return EXIT_SUCCESS;
 }
